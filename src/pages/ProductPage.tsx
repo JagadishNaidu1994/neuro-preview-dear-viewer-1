03c6412a2787079619d32c60ac35866e3a59109a
Bondada Jagadish <50915479+JagadishNaidu1994@users.noreply.github.com>
2025-06-26 00:56:27 +0530

    Updated ProductPage.tsx

--- src/pages/ProductPage.tsx
+++ src/pages/ProductPage.tsx
@@ -38,6 +38,8 @@ const ProductDetail = () => {
   }
 
   return (
+          <Header />
+
     <div className="bg-[#F8F8F5] min-h-screen">
       <Header />
       <div className="max-w-[1300px] mx-auto px-4 md:px-8 py-16 grid grid-cols-1 md:grid-cols-2 gap-12 items-center">
