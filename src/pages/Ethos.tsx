f0330a58799ee01c4284dcb185fd8983089677cf
Bondada Jagadish <50915479+JagadishNaidu1994@users.noreply.github.com>
2025-06-25 22:42:46 +0530

    Updated Ethos.tsx

--- src/pages/Ethos.tsx
+++ src/pages/Ethos.tsx
@@ -3,6 +3,8 @@ import Header from "@/components/Header";
 const Ethos = () => {
   return (
     <div className="min-h-screen bg-[#F8F8F5] p-8">
+                <Header />
+
       <div className="max-w-[1905px] mx-auto">
         <h1 className="text-6xl font-normal text-[#1E1E1E] mb-8">Our Ethos</h1>
         <p className="text-xl text-[#231F20] mb-16">
