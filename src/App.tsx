002b8625d474dbad42520b213412f5c563c6671c
Bondada Jagadish <50915479+JagadishNaidu1994@users.noreply.github.com>
2025-06-26 12:24:21 +0530

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,18 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 import ShopAll from "./pages/ShopAll";
 import TheScience from "./pages/TheScience";
 import Ethos from "./pages/Ethos";
 import HerbalIndex from "./pages/HerbalIndex";
-import ProductPage from "@/pages/ProductPage"; // <-- new page
-import { AuthProvider } from "./context/AuthProvider";
-import ProtectedRoute from "./components/ProtectedRoute";
+import ProductPage from "@/pages/ProductPage";
 import AccountPage from "./pages/AccountPage";
 
-
+import { AuthProvider } from "./context/AuthProvider";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 const queryClient = new QueryClient();
 
@@ -23,26 +23,27 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Index />} />
-          <Route
-            path="/account"
-            element={
-              <ProtectedRoute>
-                <AccountPage />
-              </ProtectedRoute>
-            }
-          />
-          <Route path="/shop-all" element={<ShopAll />} />
-          <Route path="/product" element={<ProductPage />} />
-          <Route path="/the-science" element={<TheScience />} />
-          <Route path="/ethos" element={<Ethos />} />
-          <Route path="/herbal-index" element={<HerbalIndex />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
+      <AuthProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Index />} />
+            <Route
+              path="/account"
+              element={
+                <ProtectedRoute>
+                  <AccountPage />
+                </ProtectedRoute>
+              }
+            />
+            <Route path="/shop-all" element={<ShopAll />} />
+            <Route path="/product" element={<ProductPage />} />
+            <Route path="/the-science" element={<TheScience />} />
+            <Route path="/ethos" element={<Ethos />} />
+            <Route path="/herbal-index" element={<HerbalIndex />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </BrowserRouter>
+      </AuthProvider>
     </TooltipProvider>
   </QueryClientProvider>
 );
