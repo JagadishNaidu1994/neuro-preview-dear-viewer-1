a763488d7476b2a986e4e74aae9ac20c46cfb529
Bondada Jagadish <50915479+JagadishNaidu1994@users.noreply.github.com>
2025-06-26 12:57:21 +0530

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,20 @@
+// src/App.tsx
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Toaster } from "@/components/ui/toaster";
-import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Index from "./pages/Index";
-import NotFound from "./pages/NotFound";
-import ShopAll from "./pages/ShopAll";
-import TheScience from "./pages/TheScience";
-import Ethos from "./pages/Ethos";
-import HerbalIndex from "./pages/HerbalIndex";
+import Index from "@/pages/Index";
+import NotFound from "@/pages/NotFound";
+import ShopAll from "@/pages/ShopAll";
+import TheScience from "@/pages/TheScience";
+import Ethos from "@/pages/Ethos";
+import HerbalIndex from "@/pages/HerbalIndex";
 import ProductPage from "@/pages/ProductPage";
-import AccountPage from "./pages/AccountPage";
+import AccountPage from "@/pages/AccountPage";
 
-import { AuthProvider } from "./context/AuthProvider";
-import ProtectedRoute from "./components/ProtectedRoute";
+import { AuthProvider } from "@/context/AuthProvider";
+import ProtectedRoute from "@/components/ProtectedRoute";
 
 const queryClient = new QueryClient();
 
@@ -22,11 +22,16 @@ const App = () => (
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
       <Toaster />
-      <Sonner />
-      <AuthProvider>
-        <BrowserRouter>
+      <BrowserRouter>
+        <AuthProvider>
           <Routes>
             <Route path="/" element={<Index />} />
+            <Route path="/shop-all" element={<ShopAll />} />
+            <Route path="/product" element={<ProductPage />} />
+            <Route path="/the-science" element={<TheScience />} />
+            <Route path="/ethos" element={<Ethos />} />
+            <Route path="/herbal-index" element={<HerbalIndex />} />
+
             <Route
               path="/account"
               element={
@@ -35,15 +40,11 @@ const App = () => (
                 </ProtectedRoute>
               }
             />
-            <Route path="/shop-all" element={<ShopAll />} />
-            <Route path="/product" element={<ProductPage />} />
-            <Route path="/the-science" element={<TheScience />} />
-            <Route path="/ethos" element={<Ethos />} />
-            <Route path="/herbal-index" element={<HerbalIndex />} />
+
             <Route path="*" element={<NotFound />} />
           </Routes>
-        </BrowserRouter>
-      </AuthProvider>
+        </AuthProvider>
+      </BrowserRouter>
     </TooltipProvider>
   </QueryClientProvider>
 );
