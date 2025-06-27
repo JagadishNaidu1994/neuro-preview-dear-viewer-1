7459dc203a27dbe8884c31d5b94cac7bbe6e15a7
Bondada Jagadish <50915479+JagadishNaidu1994@users.noreply.github.com>
2025-06-27 14:27:57 +0530

    Updated main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,12 @@
+// import { createRoot } from "react-dom/client";
+// import App from "./App.tsx";
+// import "./index.css";
+
+// createRoot(document.getElementById("root")!).render(<App />);
+
+
+
+
 // src/main.tsx
 import React from "react";
 import { createRoot } from "react-dom/client";
