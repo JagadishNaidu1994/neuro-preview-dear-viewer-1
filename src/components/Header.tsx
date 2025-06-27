dc1c2de6af43e50c6a5a19fc50cb1dd0865b5797
Bondada Jagadish <50915479+JagadishNaidu1994@users.noreply.github.com>
2025-06-27 16:14:40 +0530

    Updated Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,6 +3,8 @@ import { Link, useNavigate } from "react-router-dom";
 import { useAuth } from "@/context/AuthProvider";
 import { FaBars, FaTimes, FaUser, FaShoppingCart, FaFlask, FaLeaf, FaBookOpen, FaHome } from "react-icons/fa";
 import AuthModal from "@/components/AuthModal";
+import MobileNav from "./MobileNav";
+
 
 const Header = () => {
   const { user } = useAuth();
