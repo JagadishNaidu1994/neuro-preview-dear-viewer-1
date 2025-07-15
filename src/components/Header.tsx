ca31002f5004b6904cc9503987a1c49508db0088
Bondada Jagadish <50915479+JagadishNaidu1994@users.noreply.github.com>
2025-07-15 17:34:59 +0530

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -103,19 +103,10 @@ const Header = forwardRef<HTMLButtonElement>((_, ref) => {
                 </Button>
               </motion.div>
 
+            
               
-              
-              
-<!--               <Button variant="ghost" size="sm" onClick={openCart} className="relative p-2" ref={ref}>
-                <ShoppingCart className="w-10 h-10" />
-                {totalItems > 0 && (
-                  <span className="absolute -top-1 -right-1 bg-[#514B3D] text-white text-xs rounded-full w-5 h-5 flex items-center justify-center">
-                    {totalItems}
-                  </span>
-                )}
-              </Button> -->
-
-              
+           
+            
               
               
               {/* User Account */}
